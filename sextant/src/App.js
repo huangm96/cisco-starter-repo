28e8f2ebbca3d0b70e83476a69eb50bb35229cd6
Min-Huang <minh.huang96@gmail.com>
2022-12-28 17:30:06 -0800

    updated Exhibit.js

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -1,13 +1,18 @@
 import "./App.css";
 import Banner from "./components/Banner";
 import Exhibit from "./components/Exhibit";
+import GetIpAddress from "./components/GetIpAddress";
 function App() {
   return (
     <div className="App">
       <Banner title="Sextant" />
       <div className="main-content">
-        <Exhibit name="IPV4" />
-        <Exhibit name="IPV6" />
+        <Exhibit name="IPV4">
+          <GetIpAddress type="IPV4" />
+        </Exhibit>
+        <Exhibit name="IPV6">
+          <GetIpAddress type="IPV6" />
+        </Exhibit>
       </div>
     </div>
   );
--- sextant/src/components/Exhibit.js
+++ sextant/src/components/Exhibit.js
@@ -1,11 +1,10 @@
 import React from "react";
 import "./Exhibit.css";
-import GetIpAddress from "./GetIpAddress";
-function Exhibit({ name }) {
+function Exhibit({ name, children }) {
   return (
     <div className="exhibit-container">
       <h3 className="exhibit-name">{name}</h3>
-      <GetIpAddress type={name.toUpperCase()} />
+      <>{children}</>
     </div>
   );
 }
